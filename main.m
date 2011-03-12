fba51df67fab9111e214f1748907fdd22836230d
Jim <countingtoten@gmail.com>
2011-03-12 15:36:17 -0600

    Change company title

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  BetterDictionary
 //
 //  Created by James Weinert on 2/12/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Weinert Works. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
